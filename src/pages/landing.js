4ba92275fd9ae7ccfae7e826065f9321f2f7e1ac
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-08 22:59:03 -0400

    Update landing.js

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -4,11 +4,5 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import BannerLanding from '../components/BannerLanding'
 
-import pic08 from '../assets/images/pic08.jpg'
-import pic09 from '../assets/images/pic09.jpg'
-import pic10 from '../assets/images/pic10.jpg'
-
-const Landing = (props) => (
-
 
 export default Landing
