a9f454d758a691fedb96cd8f155e9b6946178933
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-09 12:45:20 -0400

    Update aboutus.js

--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -1,18 +1,28 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
+import BannerLanding from '../components/BannerLanding'
 
 import pic08 from '../assets/images/pic08.jpg'
 import pic09 from '../assets/images/pic09.jpg'
 import pic10 from '../assets/images/pic10.jpg'
 import pic11 from '../assets/images/pic11.jpg'
 
-const Elements = (props) => (
+
+const Landing = (props) => (
+    <Layout>
+        <Helmet>
+            <title>About Us</title>
+            <meta name="description" content="Landing Page" />
+        </Helmet>
+
+        <BannerLanding />
     <Layout>
         <Helmet>
             <title>About US</title>
             <meta name="description" content="About Us" />
-        </Helmet>
+      
 
        
                                 <h4>Left &amp; Right</h4>
