77354c57f62bc4735514c58f6f01f577668ad24c
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-09 12:00:48 -0400

    Update aboutus.js

--- src/pages/aboutus.js
+++ src/pages/aboutus.js
@@ -2,22 +2,22 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 
-import pic03 from '../assets/images/pic03.jpg'
 import pic08 from '../assets/images/pic08.jpg'
-import pic13 from '../assets/images/pic13.jpg'
-import pic14 from '../assets/images/pic14.jpg'
+import pic09 from '../assets/images/pic09.jpg'
+import pic10 from '../assets/images/pic10.jpg'
+import pic11 from '../assets/images/pic11.jpg'
 
 const Elements = (props) => (
     <Layout>
         <Helmet>
             <title>About US</title>
-            <meta name="description" content="Elements Page" />
+            <meta name="description" content="About Us" />
         </Helmet>
 
        
                                 <h4>Left &amp; Right</h4>
-                                <p><span className="image left"><img src={pic13} alt="" /></span>Lorem ipsum dolor sit accumsan interdum nisi, quis tincidunt felis sagittis eget. tempus euismod. Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent tincidunt felis sagittis eget. tempus euismod. Vestibulum ante ipsum primis sagittis eget. tempus euismod. Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent tincidunt felis sagittis eget tempus vestibulum ante ipsum primis in faucibus magna blandit adipiscing eu felis iaculis.</p>
-                                <p><span className="image right"><img src={pic14} alt="" /></span>Lorem ipsum dolor sit accumsan interdum nisi, quis tincidunt felis sagittis eget. tempus euismod. Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent tincidunt felis sagittis eget. tempus euismod. Vestibulum ante ipsum primis sagittis eget. tempus euismod. Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent tincidunt felis sagittis eget tempus vestibulum ante ipsum primis in faucibus magna blandit adipiscing eu felis iaculis.</p>
+                                <p><span className="image left"><img src={pic08} alt="" /></span>Jillian Blake</p>
+                                <p><span className="image right"><img src={pic09} alt="" /></span>Ami Patel</p>
 
                                 <h3>Box</h3>
                                 <div className="box">
