ad2225a52e45118804d11173474efa5e572979fe
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-08 23:19:40 -0400

    Create BannerServices.js

---
+++ src/components/BannerServices.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const BannerServices = (props) => (
+    <section id="banner" className="style2">
+        <div className="inner">
+            <header className="major">
+                <h1>About Us</h1>
+            </header>
+            <div className="content">
+                <p>We have represented clients from all over the world.</p>
+            </div>
+        </div>
+    </section>
+)
+
+export default BannerServices
