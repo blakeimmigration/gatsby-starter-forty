e214734a23c43d1b484e6d3532fbcca60d499f07
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-09 11:12:42 -0400

    Update services.js

--- src/pages/services.js
+++ src/pages/services.js
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import BannerServices from '../components/BannerServices'
 import pic08 from '../assets/images/pic13.jpg'
-import pic09 from '../assets/images/pic09.jpg'
+import pic09 from '../assets/images/pic14.jpg'
 import pic10 from '../assets/images/pic10.jpg'
 
 const Services = (props) => (
