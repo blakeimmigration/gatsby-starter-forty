4e6bb9020d7b435e0fbdcb6044b482106a85319b
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-09 09:35:46 -0400

    Update services.js

--- src/pages/services.js
+++ src/pages/services.js
@@ -3,7 +3,7 @@ import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import BannerServices from '../components/BannerServices'
-import pic08 from '../assets/images/pic08.jpg'
+import pic08 from '../assets/images/pic14.jpg'
 import pic09 from '../assets/images/pic09.jpg'
 import pic10 from '../assets/images/pic10.jpg'
 
