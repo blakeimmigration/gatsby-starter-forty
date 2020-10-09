705b82d39131b56461e5417202bd408073380c1a
blakeimmigration <72585002+blakeimmigration@users.noreply.github.com>
2020-10-08 22:47:59 -0400

    Create BannerAboutus.js

---
+++ src/components/import React from 'react'  const BannerLanding = (props) => (     <section id="banner" className="style2">         <div className="inner">             <header className="major">                 <h1>About Us</h1>             </header>             <div className="content">                 <p>WE ARE A GROUP OF HARD-WORKING LEGAL PROFESSIONALS WHO HANDLE EACH CASE WITH COMPASSION, DILIGENCE, AND INTEGRITY.</p>             </div>         </div>     </BannerAboutus.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+
+const BannerAboutus = (props) => (
+    <section id="banner" className="style2">
+        <div className="inner">
+            <header className="major">
+                <h1>About Us</h1>
+            </header>
+            <div className="content">
+                <p>WE ARE A GROUP OF HARD-WORKING LEGAL PROFESSIONALS WHO HANDLE EACH CASE WITH COMPASSION, DILIGENCE, AND INTEGRITY.</p>
+            </div>
+        </div>
+    </section>
+)
+
+export default BannerAboutus
